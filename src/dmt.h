901a3aa9c2ccce040eb7143d021c3004f5390741
rxi <rxi@users.noreply.github.com>
2013-12-20 20:47:30 +0000

    Added license

--- src/dmt.h
+++ src/dmt.h
@@ -1,3 +1,10 @@
+/**
+ * Copyright (c) 2013, rxi
+ *
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the MIT license. See LICENSE for details.
+ */
+
 #ifndef DMT_H_
 #define DMT_H_
 
