90776e945c233a85156aa19db829e7e15db7cdb6
rxi <rxi@users.noreply.github.com>
2013-12-20 12:36:44 +0000

    Changed default binary name for example program

--- example/build_example.py
+++ example/build_example.py
@@ -4,13 +4,12 @@ import os, sys
 BIN_DIR = "../bin"
 INCLUDE_DIR = "../src"
 
-
 os.chdir(sys.path[0])
 
 if not os.path.exists(BIN_DIR):
   os.makedirs(BIN_DIR)
 
 os.system(
-  "gcc -Wall -I%s -o %s/test.bin ../src/*.c example.c"
+  "gcc -Wall -I%s -o %s/example.bin ../src/*.c example.c"
   % (INCLUDE_DIR, BIN_DIR))
 
