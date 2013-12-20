ed52d3c910f8466003cabecd689a14f44556b630
rxi <rxi@users.noreply.github.com>
2013-12-20 12:20:10 +0000

    Changed example build script to create bin dir
    Changed example build script to create the bin directory its going to
    output to if it does not already exist.

--- example/build_example.py
+++ example/build_example.py
@@ -1,6 +1,16 @@
 #!/usr/bin/python
 import os, sys
 
+BIN_DIR = "../bin"
+INCLUDE_DIR = "../src"
+
+
 os.chdir(sys.path[0])
-os.system("gcc -Wall -I../src -o ../bin/test.bin ../src/*.c example.c")
+
+if not os.path.exists(BIN_DIR):
+  os.makedirs(BIN_DIR)
+
+os.system(
+  "gcc -Wall -I%s -o %s/test.bin ../src/*.c example.c"
+  % (INCLUDE_DIR, BIN_DIR))
 
